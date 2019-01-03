a600f0d89236bc4715110c1dbaaf6dbf838bfa08
Erin Green <erin.y.green@gmail.com>
2019-01-03 20:28:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
   
   get '/articles/:id' do
     
-    @article = Article.find(params[:id])
+    @current_article = Article.find(params[:id])
     
     erb :show 
   end
